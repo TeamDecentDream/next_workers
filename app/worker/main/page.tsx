53d46e921746de747c5c95990010a7781413acc3
KimKyeongSeog <kimks9241@naver.com>
2024-02-05 16:57:59 +0900

    main_layout

--- app/worker/main/page.tsx
+++ app/worker/main/page.tsx
@@ -12,48 +12,43 @@ const Main: FC = () => {
         <div className="">
           <Navbar />
         </div>
-        <main className="bg-red-100 w-[1440px] h-[1024px] grow">
+        <main className="w-[1440px] h-full grow">
           <div>
-            <div className="bg-yellow-200 w-24 text-2xl mx-6 my-6">
-              공지사항
-            </div>
+            <div className="font-bold w-32 text-3xl mx-6 mt-12">공지사항</div>
             <div className="ml-[90%]">
               <OnDate />
             </div>
-            <div className="bg-blue-200 mx-6 my-6">
-              <div className="lg:ml-16 mb-6 w-32">전직원 공지사항</div>
-              <div className="flex gap-8 justify-between">
-                <div className="w-[400px] h-[200px] bg-green-100 ml-20">
+            <div className="mx-6 my-6">
+              <div className="font-semibold text-lg lg:ml-16 mb-6 w-36">
+                전직원 공지사항
+              </div>
+              <div className="flex justify-between">
+                <div className="w-[480px] h-[300px] bg-green-100 mx-auto">
                   게시글
                 </div>
                 <div className="w-0 border-x-2 border-dashed border-red-500"></div>
-                <div className="w-[400px] h-[200px] bg-green-100 mr-20">
+                <div className="w-[480px] h-[300px] bg-green-100 mx-auto">
                   긴급특이사항
                 </div>
               </div>
             </div>
           </div>
           <div>
-            <div className="bg-yellow-200 w-24 text-2xl mx-6 my-6">
-              농장상황
-            </div>
-            <div className="bg-blue-200 mx-6 my-6">
-              <div className="lg:ml-16 mb-6 w-32">전직원 공지사항</div>
+            <div className="font-bold w-32 text-3xl mx-6 my-6">농장상황</div>
+            <div className="mx-6 my-12">
               <div className="flex gap-8 justify-between">
-                <div className="w-[400px] h-[200px] bg-green-100 ml-20">
+                <div className="w-[480px] h-[300px] bg-green-100 mx-auto">
                   예상 수확량 등
                 </div>
                 <div className="w-0 border-x-2 border-dashed border-red-500"></div>
-                <div className="w-[400px] h-[200px] bg-green-100 mr-20">
+                <div className="w-[480px] h-[300px] bg-green-100 mx-auto">
                   농가 온도계 등
                 </div>
               </div>
             </div>
           </div>
           <div>
-            <div className="bg-yellow-200 w-24 text-2xl mx-6 my-6">
-              주간예보
-            </div>
+            <div className="font-bold w-32 text-3xl mx-6 my-6">주간예보</div>
             <div className="w-800 h-48 items-center text-center bg-red-300">
               주간 예보 데이터 svg 정리예정
             </div>
--- app/worker/notice/page.tsx
+++ app/worker/notice/page.tsx
@@ -9,9 +9,25 @@ const Notice: FC = () => {
   return (
     <div>
       <div className="flex">
-        <Navbar />
-        <main>
-          <OnDate />
+        <div className="">
+          <Navbar />
+        </div>
+        <main className="w-[1440px] h-full grow">
+          <div className="font-bold w-32 text-3xl mx-6 mt-6">공지사항</div>
+          <div className="ml-[90%]">
+            <OnDate />
+          </div>
+          <div className="">
+            <div>1월 평가 우수 직원 보너스 안내</div>
+            <div className="flex gap-8">
+              <div>날짜</div>
+              <div>작성자</div>
+            </div>
+            <div className="bg-red-100 mx-32 h-96">게시글 내용</div>
+          </div>
+          <div>
+            <div>공지사항 List</div>
+          </div>
         </main>
       </div>
       <Footer />
