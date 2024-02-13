33b9f842bd0d3d0c85fdccf687721c1c2e207e0f
GRyu1 <heliotrop1024@gmail.com>
2024-02-13 17:32:38 +0900

    농부평가, 글알림

---
+++ app/worker/evaluation/page.tsx
@@ -0,0 +1,391 @@
+"use client";
+
+import Footer from "@/src/components/footer/Footer";
+import OnDate from "@/src/components/functional/OnDate";
+import Navbar from "@/src/components/navbar/Navbar";
+import { ChangeEvent, FC, useState } from "react";
+
+const Significant: FC = () => {
+  const [workJournals, setWorkJournals] = useState<Array<number>>([0,0,0,0,0,0]);
+
+  const handleRadioChange = (index: number, score: number) => {
+    setWorkJournals(prevWorkJournals => {
+      const updatedWorkJournals = [...prevWorkJournals];
+      updatedWorkJournals[index] = score;
+      return updatedWorkJournals;
+    });
+    console.log(workJournals)
+  };
+
+  return (
+    <div className="min-w-[1440px] w-full min-h-[900px] flex h-screen">
+      <Navbar />
+
+      <main className="min-w-[1140px] w-full h-full flex flex-col">
+        <div className="font-bold w-64 text-3xl mx-6 mt-6">농부평가</div>
+        <div className="ml-[90%]">
+          <OnDate />
+        </div>
+        <div>
+          <div className="ml-16 mb-4 flex items-start flex-col">
+            <p className="text-xl font-semibold">평가 대상 : ㅁㅁㅁ님</p>
+          </div>
+          <div className="flex ml-24 gap-4 mb-6">
+            <div>날짜</div>
+            <div>작성자</div>
+          </div>
+          <div className="mx-32 h-96 overflow-y-auto bg-slate-100 p-4 rounded-[20px] flex">
+            <div className="h-full w-1/2">
+              <div className="w-full h-1/3">
+                <p>
+                  작업 일지를 정확하게 작성하고 일정을 지키는 데 신경 쓰고 있나?
+                </p>
+                <div className="flex justify-between p-4">
+                <label>
+                  <input
+                    type="radio"
+                    name="work_journal_0"
+                    value="부족함"
+                    onChange={()=>{handleRadioChange(0,1)}}
+                    checked={workJournals[0] === 1}
+                  />
+                  부족함
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="work_journal_0"
+                    value="조금 부족함"
+                    onChange={()=>{handleRadioChange(0,2)}}
+                    checked={workJournals[0] === 2}
+                  />
+                  조금 부족함
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="work_journal_0"
+                    value="보통"
+                    onChange={()=>{handleRadioChange(0,3)}}
+                    checked={workJournals[0] === 3}
+                  />
+                  보통
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="work_journal_0"
+                    value="조금 잘함"
+                    onChange={()=>{handleRadioChange(0,4)}}
+                    checked={workJournals[0] === 4}
+                  />
+                  조금 잘함
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="work_journal_0"
+                    value="잘함"
+                    onChange={()=>{handleRadioChange(0,5)}}
+                    checked={workJournals[0] === 5}
+                  />
+                  잘함
+                </label>
+                </div>
+              </div>
+              <div className="w-full h-1/3">
+                <p>농부는 농작물을 관리하고 유지보수하는 데 꼼꼼한가?</p>
+                <div className="flex justify-between p-4">
+                <label>
+                  <input
+                    type="radio"
+                    name="work_journal_1"
+                    value="부족함"
+                    onChange={()=>{handleRadioChange(1,1)}}
+                    checked={workJournals[1] === 1}
+                  />
+                  부족함
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="work_journal_1"
+                    value="조금 부족함"
+                    onChange={()=>{handleRadioChange(1,2)}}
+                    checked={workJournals[1] === 2}
+                  />
+                  조금 부족함
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="work_journal_1"
+                    value="보통"
+                    onChange={()=>{handleRadioChange(1,3)}}
+                    checked={workJournals[1] === 3}
+                  />
+                  보통
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="work_journal_1"
+                    value="조금 잘함"
+                    onChange={()=>{handleRadioChange(1,4)}}
+                    checked={workJournals[1] === 4}
+                  />
+                  조금 잘함
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="work_journal_1"
+                    value="잘함"
+                    onChange={()=>{handleRadioChange(1,5)}}
+                    checked={workJournals[1] === 5}
+                  />
+                  잘함
+                </label>
+                </div>
+                
+              </div>
+              <div className="w-full h-1/3">
+                <p>새로운 농업 기술 및 방법을 학습하고 적용하는 데 적극적인가?</p>
+                <div className="flex justify-between p-4">
+                <label>
+                  <input
+                    type="radio"
+                    name="work_journal_2"
+                    value="부족함"
+                    onChange={()=>{handleRadioChange(2,1)}}
+                    checked={workJournals[2] === 1}
+                  />
+                  부족함
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="work_journal_2"
+                    value="조금 부족함"
+                    onChange={()=>{handleRadioChange(2,2)}}
+                    checked={workJournals[2] === 2}
+                  />
+                  조금 부족함
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="work_journal_2"
+                    value="보통"
+                    onChange={()=>{handleRadioChange(2,3)}}
+                    checked={workJournals[2] === 3}
+                  />
+                  보통
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="work_journal_2"
+                    value="조금 잘함"
+                    onChange={()=>{handleRadioChange(2,4)}}
+                    checked={workJournals[2] === 4}
+                  />
+                  조금 잘함
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="work_journal_2"
+                    value="잘함"
+                    onChange={()=>{handleRadioChange(2,5)}}
+                    checked={workJournals[2] === 5}
+                  />
+                  잘함
+                </label>
+                </div>
+              </div>
+            </div>
+            <div className="h-full w-1/2 ">
+              <div className="w-full h-1/3">
+                <p>특이사항이 발생 할 때의 대처가 적절한가?</p>
+                <div className="flex justify-between p-4">
+                <label>
+                  <input
+                    type="radio"
+                    name="work_journal_3"
+                    value="부족함"
+                    onChange={()=>{handleRadioChange(3,1)}}
+                    checked={workJournals[3] === 1}
+                  />
+                  부족함
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="work_journal_3"
+                    value="조금 부족함"
+                    onChange={()=>{handleRadioChange(3,2)}}
+                    checked={workJournals[3] === 2}
+                  />
+                  조금 부족함
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="work_journal_3"
+                    value="보통"
+                    onChange={()=>{handleRadioChange(3,3)}}
+                    checked={workJournals[3] === 3}
+                  />
+                  보통
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="work_journal_3"
+                    value="조금 잘함"
+                    onChange={()=>{handleRadioChange(3,4)}}
+                    checked={workJournals[3] === 4}
+                  />
+                  조금 잘함
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="work_journal_3"
+                    value="잘함"
+                    onChange={()=>{handleRadioChange(3,5)}}
+                    checked={workJournals[3] === 5}
+                  />
+                  잘함
+                </label>
+                </div>
+              </div>
+              <div className="w-full h-1/3">
+                <p>출/퇴근 시간이 정확한가?</p>
+                <div className="flex justify-between p-4">
+                <label>
+                  <input
+                    type="radio"
+                    name="work_journal_4"
+                    value="부족함"
+                    onChange={()=>{handleRadioChange(4,1)}}
+                    checked={workJournals[4] === 1}
+                  />
+                  부족함
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="work_journal_4"
+                    value="조금 부족함"
+                    onChange={()=>{handleRadioChange(4,2)}}
+                    checked={workJournals[4] === 2}
+                  />
+                  조금 부족함
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="work_journal_4"
+                    value="보통"
+                    onChange={()=>{handleRadioChange(4,3)}}
+                    checked={workJournals[4] === 3}
+                  />
+                  보통
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="work_journal_4"
+                    value="조금 잘함"
+                    onChange={()=>{handleRadioChange(4,4)}}
+                    checked={workJournals[4] === 4}
+                  />
+                  조금 잘함
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="work_journal_4"
+                    value="잘함"
+                    onChange={()=>{handleRadioChange(4,5)}}
+                    checked={workJournals[4] === 5}
+                  />
+                  잘함
+                </label>
+                </div>
+                </div>
+              <div className="w-full h-1/3">
+                <p>다른 농부와의 관계가 우호적인가?</p>
+                <div className="flex justify-between p-4">
+                <label>
+                  <input
+                    type="radio"
+                    name="work_journal_5"
+                    value="부족함"
+                    onChange={()=>{handleRadioChange(5,1)}}
+                    checked={workJournals[5] === 1}
+                  />
+                  부족함
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="work_journal_5"
+                    value="조금 부족함"
+                    onChange={()=>{handleRadioChange(5,2)}}
+                    checked={workJournals[5] === 2}
+                  />
+                  조금 부족함
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="work_journal_5"
+                    value="보통"
+                    onChange={()=>{handleRadioChange(5,3)}}
+                    checked={workJournals[5] === 3}
+                  />
+                  보통
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="work_journal_5"
+                    value="조금 잘함"
+                    onChange={()=>{handleRadioChange(5,4)}}
+                    checked={workJournals[5] === 4}
+                  />
+                  조금 잘함
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="work_journal_5"
+                    value="잘함"
+                    onChange={()=>{handleRadioChange(5,5)}}
+                    checked={workJournals[5] === 5}
+                  />
+                  잘함
+                </label>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr className="mt-6 border-1 border-gray-300 border-solid" />
+        <div>
+          <div className="relative mx-32 h-64 mt-6 flex flex-col">
+            <h1 className="text-xl font-bold">비고</h1>
+            <textarea className="grow border-2 border-gray-300 border-solid resize-none p-4 focus:outline-gray-300  rounded-[20px]"></textarea>
+            <button className="w-full border-2 border-gray-500 border-solid rounded-md">제출하기</button>
+          </div>
+        </div>
+        <Footer />
+      </main>
+    </div>
+  );
+};
+
+export default Significant;
--- app/worker/significant/page.tsx
+++ app/worker/significant/page.tsx
@@ -7,32 +7,47 @@ import { FC } from "react";
 
 const Significant: FC = () => {
   return (
-    <div>
-      <div className="flex">
-        <div className="">
-          <Navbar />
-        </div>
-        <main className="w-[1440px] h-full grow">
-          <div className="font-bold w-64 text-3xl mx-6 mt-6">특이사항 보고</div>
+    
+      <div className="min-w-[1440px] w-full min-h-[900px] flex h-screen">
+        
+        <Navbar />
+        
+        <main className="min-w-[1140px] w-full h-full flex flex-col">
+          <div className="font-bold w-64 text-3xl mx-6 mt-6">특이사항 목록</div>
           <div className="ml-[90%]">
             <OnDate />
           </div>
           <div>
-            <div className="text-lg ml-16 mb-4 font-semibold">농부</div>
-            <div className="flex ml-24 gap-4 my-8">
-              <div>
-                <OnDate />
-              </div>
+            <div className="ml-16 mb-4 flex items-center">
+              <p className="text-lg font-semibold">1월 평가 우수 직원 보너스 안내</p>
+              <button className="ml-10 border-solid border-green-500 border-[1px] p-1 rounded-md text-green-500 hover:text-white hover:bg-green-500">공지올리기</button>
+              <button className="ml-2 border-solid border-red-500 border-[1px] p-1 rounded-md text-red-500 hover:text-white hover:bg-red-500">공지내리기</button>
+            </div>
+            <div className="flex ml-24 gap-4 mb-6">
+              <div>날짜</div>
               <div>작성자</div>
             </div>
-            <div className="bg-red-100 mx-32 h-[700px] mb-[100px]">
-              특이사항 보고(게시판)
+            <div className="mx-32 h-96 overflow-y-auto bg-slate-200 p-4 rounded-[20px]">게시글 내용</div>
+          </div>
+          <hr className="mt-6 border-1 border-gray-300 border-solid"/>
+          <div>
+            <div className="relative mx-32 h-64 mt-6 ">
+              <h1 className="text-xl font-bold">특이사항 List</h1>
+              <div className="absolute bottom-4 right-4 flex gap-4">
+                <button>이전</button>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>다음</button>
+              </div>
             </div>
           </div>
+          <Footer />
         </main>
       </div>
-      <Footer />
-    </div>
   );
 };
 
