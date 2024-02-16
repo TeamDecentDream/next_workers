33e7cd17db4797ff98154361d56e315fbe0708d0
KimKyeongSeog <148549192+KimKyeongSeog@users.noreply.github.com>
2024-02-16 22:24:01 +0900

    mvp_todo

--- app/admin/todo/page.tsx
+++ app/admin/todo/page.tsx
@@ -1,37 +1,72 @@
-"use client";
+"use client"
 
+import { useState } from "react";
+import { NextPage } from "next";
 import Footer from "@/src/components/footer/Footer";
 import OnDate from "@/src/components/functional/OnDate";
 import Navbar from "@/src/components/adminnavbar/Navbar";
-import { FC } from "react";
 
-const Todo: FC = () => {
+interface Todo {
+  id: number;
+  text: string;
+}
+
+const Todo: NextPage = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [inputText, setInputText] = useState<string>("");
+
+  const addTodo = () => {
+    if (inputText.trim() == "") {
+      alert("내용을 입력해주세요.")
+    }
+    else {
+      setTodos([...todos, { text: inputText, id: Date.now() }]);
+      setInputText("");
+    }
+  };
+
+  const removeTodo = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
-    <div>
-      <div className="flex">
-        <div className="">
-          <Navbar />
+    <div className="min-w-[1440px] w-full min-h-[800px] flex h-screen">
+      <Navbar />
+      <main className="min-w-[1140px] w-full h-full flex flex-col">
+        <div className="font-bold w-64 text-3xl mx-6 mt-6">할 일 관리</div>
+        <div className="ml-[90%]">
+          <OnDate />
         </div>
-        <main className="w-[1440px] h-full grow">
-          <div className="font-bold w-48 text-3xl mx-6 mt-6">할 일 관리</div>
-          <div className="ml-[90%]">
-            <OnDate />
-          </div>
-          <div>
-            <div className="text-lg ml-16 mb-4 font-semibold">스케쥴러</div>
-            <div className="flex ml-24 gap-4 my-8">
-              <div>
-                <OnDate />
-              </div>
-              <div>작성자</div>
+        <div className="bg-gray-200 w-full h-full p-12">
+          <div className="bg-gray-400 w-full h-full flex">
+            <div className="h-full w-1/2 bg-red-100">
+            
+              <input
+        type="text"
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+      />
+      <button onClick={addTodo}>할 일 추가</button>
+      
             </div>
-            <div className="bg-red-100 mx-32 h-[700px] mb-[100px]">
-              스케쥴 내용(카카오톡 캘린더 예정)
+            <div className="bg-blue-200 w-1/2">
+              <div></div>
+            <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.text}
+            <button onClick={() => removeTodo(todo.id)}>
+              삭제
+            </button>
+          </li>
+        ))}
+      </ul>
             </div>
           </div>
-        </main>
-      </div>
-      <Footer />
+          
+        </div>
+        <Footer />
+      </main>
     </div>
   );
 };
