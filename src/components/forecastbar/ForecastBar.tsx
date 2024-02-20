1914aeb9d1f890566e83c7feb9cfdeb0d4e8c6ff
KimKyeongSeog <kimks9241@naver.com>
2024-02-20 11:05:38 +0900

    min_max_temperture
    온도설정 완료

--- src/components/forecastbar/ForecastBar.tsx
+++ src/components/forecastbar/ForecastBar.tsx
@@ -1,4 +1,3 @@
-"use client";
 import axios from "axios";
 import { FC, useEffect, useState } from "react";
 
@@ -29,27 +28,50 @@ const ForecastBar: FC = () => {
   }, []);
 
   return (
-    <div className="flex flex-col">
+    <div>
       {forecastData ? (
-        <>
-          {[...Array(5)].map((_, dayIndex) => {
+        <div className=" grid grid-cols-5 h-[311px] px-32">
+          {Array.from({ length: 5 }, (_, dayIndex) => {
             const startDate = new Date();
             startDate.setDate(startDate.getDate() + dayIndex);
-            const endDate = new Date(startDate);
-            endDate.setDate(startDate.getDate() + 1);
-            const dailyForecast = forecastData.list.filter((item: any) => {
+            const startOfNoon = new Date(startDate);
+            startOfNoon.setHours(12, 0, 0, 0);
+
+            const endOfMidnight = new Date(startDate);
+            endOfMidnight.setHours(23, 59, 59, 999);
+
+            const dailyForecasts = forecastData.list.filter((item: any) => {
               const forecastDate = new Date(item.dt * 1000);
-              return forecastDate >= startDate && forecastDate < endDate;
-            })[0];
+              return (
+                forecastDate >= startOfNoon && forecastDate <= endOfMidnight
+              );
+            });
+
+            let minTemp = Infinity;
+            let maxTemp = -Infinity;
+
+            dailyForecasts.forEach((forecast: any) => {
+              minTemp = Math.min(minTemp, forecast.main.temp_min);
+              maxTemp = Math.max(maxTemp, forecast.main.temp_max);
+            });
+
+            console.log("최저 온도:", minTemp);
+            console.log("최고 온도:", maxTemp);
             return (
-              <div key={dayIndex} className="forecast-item flex">
-                <div>{startDate.toLocaleDateString().substring(2)}</div>
-                <div className="">
-                  {dailyForecast ? (
-                    <>
-                      <div>- 최저: {dailyForecast.main.temp_min}℃</div>
-                      <div>- 최고: {dailyForecast.main.temp_max}℃</div>
-                    </>
+              <div
+                key={dayIndex}
+                className="forecast-item bg-red-100 w-[150px] h-[200px]"
+              >
+                <div className="bg-red-300 날씨 카드 flex flex-col items-center w-[150px] h-[220px]">
+                  <div className=" 오늘 아이콘 카드">
+                    <div className="w-32 h-32 bg-blue-100">이미지</div>
+                    <div>{startDate.toLocaleDateString().substring(2)}</div>
+                  </div>
+                  {dailyForecasts ? (
+                    <div className="오늘 최저, 최저기온">
+                      <div>- 최저: {minTemp}℃</div>
+                      <div>- 최고: {maxTemp}℃</div>
+                    </div>
                   ) : (
                     <div>데이터 없음</div>
                   )}
@@ -57,7 +79,7 @@ const ForecastBar: FC = () => {
               </div>
             );
           })}
-        </>
+        </div>
       ) : (
         <div>Loading...</div>
       )}
